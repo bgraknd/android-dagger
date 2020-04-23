c0b0c7db8b5aa608ac098bd8a931c9813c5c9ad3
bugra <bgraknd@gmail.com>
2020-04-24 00:20:35 +0300

    Completed part 8

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,15 +1,25 @@
 package com.example.android.dagger.di
 
+import android.content.Context
+import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
 /**
  * Created by Bugra on 21.04.2020.
  */
 
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
 
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
     fun inject(activity: RegistrationActivity)
+
+    fun inject(activity: MainActivity)
 
 }
